a5a7983cdcd7152ab06a1fc6d0c32b0da02eb7ef
lokehoke <ibymif2000@yandex.ru>
2020-09-19 00:21:33 +0300

    iomanip

--- iomanip/include/iomanip.hpp
+++ iomanip/include/iomanip.hpp
@@ -1,23 +1,108 @@
 /* Custom manipulators.
- * @file
- * @date 2018-08-05
- * @author Anonymous
+ * @file iomanip
+ * @date 2020
+ * @author lokehoke
  */
 
 #ifndef __IOMANIP_HPP__
 #define __IOMANIP_HPP__
 
-#include <iostream>
+#include <ostream>
 
 //{ endm manipulator
 
+template <class charT, class traits>
+decltype(auto) endm(std::basic_ostream<charT,traits>& os) {
+    for (const auto c : "[eol]\n") {
+        if (c != '\0') {
+            os.put(os.widen(c));
+        }
+    }
+
+    return os.flush();
+}
+
 //}
+
 
 //{ squares manipulator
 
+class SquaresContainer {
+    private:
+        std::ostream* m_os;
+
+    public:
+        SquaresContainer(std::ostream* os)
+            : m_os(os)
+        {}
+
+        template<
+            typename T,
+            typename = decltype(
+                std::declval<std::ostream>() << std::declval<T>()
+            )
+        >
+        decltype(auto) operator<<(const T& str) {
+            return ((*m_os) << '[' << str << ']');
+        }
+};
+
+struct Square {
+    friend decltype(auto) operator<<(std::ostream& os, Square) {
+        return SquaresContainer(&os);
+    }
+};
+
+const Square squares;
 //}
 
+
 //{ add manipulator
+
+template<
+    typename T,
+    typename = decltype(
+        std::declval<T>() + std::declval<T>()
+    )
+>
+class AddContainerSecond {
+    private:
+        std::ostream* m_os;
+        T             m_s1;
+
+    public:
+        AddContainerSecond(std::ostream* os, const T& s1)
+            : m_os(os)
+            , m_s1(s1)
+        {}
+
+        decltype(auto) operator<<(const T& s2) {
+            return (*m_os) << m_s1 + s2;
+        }
+};
+
+class AddContainerFirst {
+    private:
+        std::ostream* m_os;
+
+    public:
+        AddContainerFirst(std::ostream* os)
+            : m_os(os)
+        {}
+
+        template<typename T>
+        friend decltype(auto) operator<<(AddContainerFirst f1, const T& s1) {
+            return AddContainerSecond(f1.m_os, s1);
+        }
+};
+
+struct Add {
+    friend decltype(auto) operator<<(std::ostream& os, Add) {
+        return AddContainerFirst(&os);
+    }
+};
+
+const Add add;
 
 //}
 
