b91367eec4fb46cbb3ed4a41a278243750790f72
lokehoke <ibymif2000@yandex.ru>
2020-09-26 02:13:37 +0300

    iterator

--- iterator/include/iterator.hpp
+++ iterator/include/iterator.hpp
@@ -14,48 +14,104 @@
 
 #include <boost/iterator/iterator_adaptor.hpp>
 
-//{ image iterator
-template<class Iterator>
-class image_iterator: public boost::iterator_adaptor<...>
-{
+template< typename Container = std::vector<uint8_t> >
+class image {
+    public:
+        template< typename Iterator >
+        class image_iterator
+            : public boost::iterator_adaptor< 
+                image_iterator<Iterator>,
+                Iterator 
+            > {
+            public:
+                using TAdapter = boost::iterator_adaptor<image_iterator<Iterator>, Iterator>;
 
-};
-//}
+                image_iterator(Iterator    it,
+                               std::size_t w,
+                               std::size_t st,
+                               std::size_t shift = 0,
+                               std::size_t cur_h = 0
+                              )
+                    : TAdapter(it)
+                    , m_width(w)
+                    , m_stride(st)
+                    , m_trash(st - w)
+                    , m_shift(shift)
+                    , m_current_height(cur_h)
+                {}
 
-template<class Container = std::vector<uint8_t>>
-class image
-{
-public:
-    image(size_t width, size_t height, size_t stride):
-        data(stride * height),
-        width_(width),
-        stride_(stride)
-    {}
+                void increment() {
+                    if (m_shift < m_width - 1) {
+                        ++m_shift;
+                        ++this->base_reference();
+                    } else {
+                        m_shift = 0;
+                        ++m_current_height;
+                        this->base_reference() += (m_trash + 1);
+                    }
+                }
 
-    auto begin()
-    {
-        return image_iterator(data.begin(), width_, stride_);
-    }
+                void decrement() {
+                    if (m_shift > 0) {  
+                        --m_shift;
+                        --this->base_reference();
+                    } else {
+                        m_shift = m_width - 1;
+                        --m_current_height;
+                        this->base_reference() -= (m_trash + 1);
+                    }
+                }
 
-    auto end()
-    {
-        return image_iterator(data.end(), width_, stride_);
-    }
+                void advance(const typename TAdapter::difference_type n) {
+                    if (n > 0) {
+                        for (std::size_t i = 0; i < static_cast<std::size_t>(n); ++i) {
+                            increment();
+                        }                   
+                    } else {
+                        for (std::size_t i = 0; i < static_cast<std::size_t>(-n); ++i) {
+                            decrement();
+                        }
+                    }
+                }
 
-    auto begin() const
-    {
-        return image_iterator(data.begin(), width_, stride_);
-    }
+                typename TAdapter::difference_type distance_to(const image_iterator& it) const {
+                    return (it.m_current_height - m_current_height) * m_width + (it.m_shift - m_shift);
+                }
 
-    auto end() const
-    {
-        return image_iterator(data.end(), width_, stride_);
-    }
+            private:
+                std::size_t m_width;
+                std::size_t m_stride;
+                std::size_t m_trash;
+                std::size_t m_shift;
+                std::size_t m_current_height;
+        };
 
-private:
-    Container data; // image data including strides. Access by (x, y): data[y * stride_ + x]
-    size_t width_;
-    size_t stride_;
+        image(std::size_t width, std::size_t height, std::size_t stride)
+            : m_data(stride * height)
+            , m_width(width)
+            , m_stride(stride)
+        {}
+
+        auto begin() {
+            return image_iterator(m_data.begin(), m_width, m_stride);
+        }
+
+        auto end() {
+            return image_iterator(m_data.end(), m_width, m_stride, 0, m_data.size() / m_stride);
+        }
+
+        auto begin() const {
+            return image_iterator(m_data.begin(), m_width, m_stride);
+        }
+
+        auto end() const {
+            return image_iterator(m_data.end(), m_width, m_stride, 0, m_data.size() / m_stride);
+        }
+
+    private:
+        Container   m_data;    // image data including strides. Access by (x, y): data[y * m_stride + x]
+        std::size_t m_width;
+        std::size_t m_stride;
 };
 
 #endif // __ITERATOR_HPP__
